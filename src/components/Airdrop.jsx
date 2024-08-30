6d14fb529b035d605bb685560e58872d2c0b4695
DHRUV KHANDELWAL <khandelwaldhruv2003@gmail.com>
2024-08-30 21:10:11 +0530

    Connect-Wallet

---
+++ src/components/Airdrop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Airdrop = () => {
+  return (
+    <div>
+        <input type="text" placeholder="Amount"></input>
+        <button>Send Airdrop</button>
+    </div>
+
+  )
+}
+
+export default Airdrop
